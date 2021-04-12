f20cc26ffc270841c82a9dd9bd23c87339592751
C094 <karencsolorio@gmail.com>
2021-04-12 12:43:25 -0500

    Gale Picture

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Gale_Picture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblColorPercentage = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 38);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(134, 305);
+            this.btnScan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(130, 40);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(538, 305);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(390, 28);
+            this.progressBar.TabIndex = 3;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(692, 346);
+            this.lblProgress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(48, 13);
+            this.lblProgress.TabIndex = 4;
+            this.lblProgress.Text = "Progress";
+            // 
+            // menuMain
+            // 
+            this.menuMain.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuMain.Size = new System.Drawing.Size(995, 24);
+            this.menuMain.TabIndex = 5;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Png Files|*.png";
+            this.openFileDialog1.Title = "Open PNG File";
+            // 
+            // lblColorPercentage
+            // 
+            this.lblColorPercentage.AutoSize = true;
+            this.lblColorPercentage.Location = new System.Drawing.Point(673, 368);
+            this.lblColorPercentage.Name = "lblColorPercentage";
+            this.lblColorPercentage.Size = new System.Drawing.Size(89, 13);
+            this.lblColorPercentage.TabIndex = 6;
+            this.lblColorPercentage.Text = "Color Percentage";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(538, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(390, 233);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 442);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblColorPercentage);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblColorPercentage;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gale_Picture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmpImage = (Bitmap)pictureBox1.Image;
+            Bitmap bmpImage2 = new Bitmap(bmpImage.Width, bmpImage.Height);
+            Color colCurrent;
+            string pixelInfo = "";
+            int currentPixel = 0;
+            double red = 0;
+            double blue = 0;
+            double green = 0;
+            double redPixel = 0;
+            double otherColor = 0;
+            double percentage = 0;
+            
+
+            //Determine how many pixels we need to process
+            int maxPixels = bmpImage.Height * bmpImage.Width;
+
+            //Set max of the progressbar to Pixel Count
+            progressBar.Maximum = maxPixels + 1;
+
+            //Reset Progress Bar
+            progressBar.Value = 0;
+
+            //Clear Listbox
+            //colorDataList.Items.Clear();
+
+            //Walk over every pixel in the bitmap
+            for(int y=0;y<bmpImage.Height; y++)
+            {
+                for(int x =0; x<bmpImage.Width; x++)
+                {
+                    //Get Color of Pixel
+                    colCurrent = bmpImage.GetPixel(x, y);
+
+                    //Build String of Pixel Info
+                    pixelInfo = string.Format($" Pixel {currentPixel} Coord = [{x}, {y}]  Color RGB = {colCurrent.R}, {colCurrent.G}, {colCurrent.B}");
+
+                    //Assign color variable to the color current
+                    red = colCurrent.R;
+                    blue = colCurrent.B;
+                    green = colCurrent.G;
+
+                    //Check for red pixels
+                    if(red >= 70 && blue <= 100 && green <= 100)
+                    {
+                        redPixel++;
+                        bmpImage2.SetPixel(x, y, colCurrent);
+                    }
+                    else
+                    {
+                        otherColor++;
+                    }
+
+                    //Increment the current pixel
+                    currentPixel++;
+
+                    //Increase Value of the Progress Bar
+                    progressBar.Value = currentPixel;
+
+                    //Add to list
+                    //colorDataList.Items.Add(pixelInfo);
+
+                }//End of for loop
+                lblProgress.Text = string.Format($"{currentPixel}/{maxPixels} processed");
+            }
+            //Show image in Picture Box 1
+            pictureBox1.Image = bmpImage;
+
+            //Transfer specific color to Picture Box 2
+            pictureBox2.Image = bmpImage2;
+
+            //Percentage of red pixels
+            percentage = redPixel / otherColor;
+
+            //Exhibit the percentage of the red pixels to the Color Percentage label
+            lblColorPercentage.Text = $"Red Pixels: {redPixel}  \t Red Color Percentage: {percentage * 100:F2}%";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Show Dialog Box
+            DialogResult dgrTemp = openFileDialog1.ShowDialog();
+
+            //Check if User Selected "Open"
+            if(dgrTemp == DialogResult.OK)
+            {
+                //Use Slected File Top Open Bitmap
+                Bitmap bmpTemp = new Bitmap(openFileDialog1.FileName);
+
+                //Set BitMap to Picture Box
+                pictureBox1.Image = bmpTemp;
+            }//end if
+        }//end event
+    }
+}
